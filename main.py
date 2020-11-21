3595027d3e86db527e99f987f0dfb408d45e2079
KenjiMiyazaki <m5514096@yahoo.co.jp>
2020-11-22 00:36:30 +0900

    enumerate()関数と f-stringを使用するように修正

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# coding: utf-8
-
 import glob
 import os
 
 # 拡張子.pngの画像ファイルを取得する
 path = './dir/*.png'
-i = 1
 
 # 画像ファイルを取得する
 before_file_list = glob.glob(path)
@@ -13,10 +10,9 @@ print('変更前')
 print(before_file_list)
 
 # ファイル名を一括で変更する
-for file in before_file_list:
-    os.rename(file, './dir/icon' + str(i) + '.png')
-    i += 1
+for i, filename in enumerate(before_file_list, 1):
+    os.rename(filename, f'./dir/icon{i}.png')
 
-after_file_ist = glob.glob(path)
+after_file_list = glob.glob(path)
 print('変更後')
-print(after_file_ist)
+print(after_file_list)
